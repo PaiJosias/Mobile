8b8bd28b6334f7b9bfb2907b0b008b06860d42f3
Pedro Garcia <32979899+PaiJosias@users.noreply.github.com>
2022-10-26 14:27:09 -0300

    Update main.dart

--- aula_05/lib/main.dart
+++ aula_05/lib/main.dart
@@ -48,7 +48,7 @@ class _HomeState extends State<Home> {
                     decoration: const InputDecoration(labelText: "Nova Tarefa", labelStyle: TextStyle(color: Colors.black)),
                   ),
                 ),
-                ElevatedButton(onPressed: addTodo, child: const Text("ADD"))
+                ElevatedButton(onPressed: addTodo, child: Text("ADD"), style: ElevatedButton.styleFrom(primary: Colors.black)),
                ],
              ),
            ),
@@ -70,14 +70,102 @@ class _HomeState extends State<Home> {
        key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
        background: Container(
          color: Colors.red,
-         child: Align(
+         child: const Align(
            alignment: Alignment(-0.9,0.9),
            child: Icon(
              Icons.delete,
              color: Colors.white,
            ))
        ),
-     );
-   }
-   
+       direction: DismissDirection.startToEnd,
+       child: CheckboxListTile(
+         title: Text(_todoList[index]["title"], style: const TextStyle(color: Colors.black),),
+         value: _todoList[index]["ok"],
+         secondary: CircleAvatar(
+           backgroundColor: Colors.black,
+           child: Icon(_todoList[index]["ok"] ? Icons.check : Icons.error, color: Colors.white),
+         ),
+         onChanged: (c){
+           checkTodo(index, c);
+         },
+       ),
+       onDismissed: (direction){
+         setState(() {
+           _lastRemoved = Map.from(_todoList[index]);
+           _lastRemovedPos = index;
+           _todoList.removeAt(index);
+           _saveData();
+
+           final snack = SnackBar(
+             content: Text("Tarefa ${_lastRemoved["title"]} removida."),
+             action: SnackBarAction(
+               label: "Desfazer",
+               onPressed: (){
+                 setState(() {
+                   _todoList.insert(_lastRemovedPos, _lastRemoved);
+                   _saveData();
+                 });
+               }),
+               duration: const Duration(seconds: 2),
+           );
+           ScaffoldMessenger.of(context).removeCurrentSnackBar();
+           ScaffoldMessenger.of(context).showSnackBar(snack);
+       });
+      },  
+    );
+  }
+
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/data.json");
+  }
+
+  Future<File> _saveData() async {
+    String data = json.encode(_todoList);
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }
+
+  Future<String> _readData() async {
+    final file = await _getFile();
+    return file.readAsString();
+  }
+
+  void addTodo() {
+    setState(() {
+      Map<String, dynamic> newTodo = Map();
+      newTodo["title"] = _todoController.text;
+      _todoController.text = "";
+      newTodo["ok"] = false;
+      _todoList.add(newTodo);
+      _saveData();
+    });
+  }
+
+  void checkTodo(index, c) {
+    setState(() {
+      _todoList[index]["ok"] = c;
+      _saveData();
+    });
+  }
+
+  Future<Null> _refresh() async {
+    await Future.delayed(const Duration(seconds: 1));
+
+    setState(() {
+      _todoList.sort((a, b) {
+        if (a["ok"] && !b["ok"])
+          return 1;
+        else if (!a["ok"] && b["ok"])
+          return -1;
+        else
+          return 0;
+      });
+
+      _saveData();
+    });
+
+    return null;
+  }
 }
+
